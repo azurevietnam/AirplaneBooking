dbc62fafbde257149337e5e20b608a8bf8690641
ledona <ledona1509@gmail.com>
2015-03-16 06:42:11 +0700

    new WebBotVietJetImpl

---
+++ Source/airlinebooking.core.ws/src/main/java/arilinebooking/core/ws/webbot/WebBotVietJetImpl.java
@@ -0,0 +1,17 @@
+package arilinebooking.core.ws.webbot;
+
+import java.util.Date;
+
+public class WebBotVietJetImpl implements WebBot {
+
+	@Override
+	public String getHtmlResult(String oriCode, String desCode, Date pickedDate, int adultNumber, int childrenNumber,
+								int infantNumber) throws Exception {
+		// TODO Auto-generated method stub
+		
+		
+		
+		return null;
+	}
+
+}
